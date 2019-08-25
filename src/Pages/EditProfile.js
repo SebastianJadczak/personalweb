bc5ed31b9e9e6ceb2b9764018185d5de785de551
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2019-08-26 00:35:26 +0200

    add Page Profile and module edit Profile

---
+++ src/Pages/EditProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditProfile = (props) => {
+    return (
+        <div className="EditProfile">
+            <p>XXXXX</p>
+            <button onClick={props.exit}>Wyjdź</button>
+        </div>
+    )
+}
+export default EditProfile;
--- src/Pages/MyAccount.js
+++ src/Pages/MyAccount.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import EditProfile from "./EditProfile"
 
-const MyAccount = () => {
-    return (
-        <p>MyAccount</p>
-    )
+
+class MyAccount extends Component {
+    state = {
+        profil: false,
+        name: "Sebastian",
+        surname: "Jadczak",
+        birthday: "21-06-1990",
+        description: "Kilka słów o mnie. Tak by ktoś mógł opisać siebie i swoje doświadczenie. W dalszej części będzie można rozbudować ten element"
+    }
+
+    handleEditProfile = () => {
+        this.setState({
+            profil: true,
+        })
+    }
+    handleCloseEditProfile = () => {
+        this.setState({
+            profil: false,
+        })
+    }
+
+    render() {
+        const { name, surname, description, birthday, profil } = this.state
+
+        return (
+            <div className="account">
+                <h2>MyAccount</h2>
+                {profil ? <EditProfile exit={this.handleCloseEditProfile} /> : false}
+                <div className="acc">
+                    <div className="mySelf"></div>
+                    <div className="data">
+                        <p className="Name">Imię: <span>{name}</span></p>
+                        <p className="Surname">Nazwisko: <span>{surname}</span></p>
+                        <p className="Birthday">Data urodzin: <span>{birthday}</span></p>
+                        <p className="Description">Opis: <span>{description}</span></p>
+                    </div>
+                    <button className="mySelf" onClick={this.handleEditProfile}>Edytuj</button>
+                </div>
+            </div>
+        )
+    }
 }
 export default MyAccount;
