d1f17fab21640807a7719748d6621055fa8e0654
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2019-08-26 20:09:10 +0200

    add styles EditProfile Page

--- src/Pages/EditProfile.js
+++ src/Pages/EditProfile.js
@@ -3,8 +3,29 @@ import React from "react";
 const EditProfile = (props) => {
     return (
         <div className="EditProfile">
-            <p>XXXXX</p>
-            <button onClick={props.exit}>Wyjdź</button>
+            <h3>Zmień Dane:</h3>
+
+            <div className="changeName">
+                <label>Podaj imię</label>
+                <input type="text" />
+            </div>
+
+            <div className="changeSurname">
+                <label>Podaj Nazwisko</label>
+                <input type="text" />
+            </div>
+
+            <div className="changeBirthday">
+                <label>Podaj datę urodzin</label>
+                <input type="text" />
+            </div>
+
+            <div className="changeDescription">
+                <label>Podaj Opis</label>
+                <textarea className="description"></textarea>
+            </div>
+
+            <button onClick={props.exit}>Zapisz i Wyjdź</button>
         </div>
     )
 }
